077058a6fafc7627093c54e5c2856480ed568d1b
chenmin <350376345@qq.com>
2018-07-26 17:32:26 +0800

    合班计划需要按照UI设计实现

--- src/components/MergeClass.vue
+++ src/components/MergeClass.vue
@@ -1,31 +1,72 @@
 <template>
-    <div>
-    <div style="margin-bottom:20px;">
-      <!-- <div v-for="(item, index) in getCourses" :key="index">
-        {{item.courseName}}
-      </div> -->
-      <span>课程</span>
-      <el-tabs tab-position="left" :before-leave="checkSelected" style="height: 200px;">
-        <el-tab-pane v-for="(item, index) in getCourses" :key="index" :label="item.courseName">
-           <el-transfer ref="transfer"
-            style="text-align: left; margin-left:20px; display: inline-block"
-            v-model="item.mergedCourses"
-            :props="{
-              key: 'id',
-              label: 'name'
-            }"
-            :format="{
-              noChecked: '${total}',
-              hasChecked: '${checked}/${total}'
-            }"
-            @left-check-change="change"
-            @change="handleChange"
-            :titles="['待合并课堂', '已合并课堂']"
-            :data="item.classes">
-          </el-transfer>
-        </el-tab-pane>
-      </el-tabs>
-      <span v-show="!isTipsShow" style="margin-left:20px;color:red;">请至少选择两个课堂合并</span>
+  <div>
+    <div class="step1-container">
+      <div class="step-title">第一步：选择课程</div>
+      <div class="search-box">
+        <el-input v-model="searchCourse" placeholder="输入课程名称" clearable prefix-icon="el-icon-search">
+        </el-input>
+      </div>
+      <div>
+        <div v-for="(item, index) in courses" :key="index" class="course-item"
+        :class="{'course-active-item': activeIndex === index}" @click="clickCourse(item, index)">
+          {{item.courseName}}
+        </div>
+      </div>
+    </div>
+    <div class="step2-container">
+      <div class="direction">
+        <span class="el-icon-arrow-right step-arrow-right"></span>
+      </div>
+      <div style="display:inline-block;">
+        <div class="step-title">第二步：选择班级</div>
+        <div class="class-teacher-container" v-if="selectedCourse.classes.length > 0">
+          <el-checkbox-group @change="checkClass" v-model="checkedClasses">
+            <el-checkbox class="class-item"
+            v-for="(item, index) in selectedCourse.classes" :key="index" :label="item">
+              {{item.className}}（{{transformTeacher(item.teachers)}}）
+            </el-checkbox>
+          </el-checkbox-group>
+        </div>
+        <div v-else class="class-teacher-container">
+          没有需要合并的班级
+        </div>
+      </div>
+    </div>
+    <div class="operation">
+      <span class="el-icon-arrow-right step-arrow-right"></span>
+      <div>
+        <div>
+          <el-button type="primary" plain :disabled="!isMergable" @click="mergeClass">
+            合并班级
+            <i class="el-icon-arrow-right"></i>
+          </el-button>
+        </div>
+        <div style="margin-top:20px;">
+          <el-button type="primary" plain :disabled="!isSplitable"  @click="splitClass">
+            <i class="el-icon-arrow-left"></i>
+            拆分班级
+          </el-button>
+        </div>
+      </div>
+       <span class="el-icon-arrow-right step-arrow-right"></span>
+    </div>
+    <div class="step3-container" >
+      <div class="step-title">已合并班级</div>
+      <div class="class-teacher-container" v-if="selectedCourse.mergedClasses.length > 0">
+        <el-checkbox-group v-model="checkedMergedClasses" @change="checkMergedClass">
+
+          <el-checkbox class="class-item" v-for="(mergedClass, index) in selectedCourse.mergedClasses" :key="index" :label="mergedClass">
+            <!-- {{mergedClass}} -->
+            <span v-for="(item, index) in mergedClass.classes" :key="index">
+              {{item.className}}（{{transformTeacher(item.teachers)}}）
+              <span v-if="index < mergedClass.classes.length - 1">/ </span>
+            </span>
+          </el-checkbox>
+        </el-checkbox-group>
+      </div>
+      <div v-else  class="class-teacher-container">
+        不存在已合并的班级
+      </div>
     </div>
   </div>
 </template>
@@ -34,71 +75,155 @@
 export default {
   data () {
     return {
+      searchCourse: '',
       selectedTeacher: '0',
-      teachers: [{id: '0', name: '张丽萍'}, {id: '1', name: '李希'}],
-      courses: {
-        '0': [
-          {
-            id: 1,
-            courseName: '音乐',
-            mergedCourses: [],
-            classes: [{id: 1, name: '音乐/一年级（1）班'}, {id: 2, name: '音乐/一年级（2）班'}]
-          },
-          {
-            id: 2,
-            courseName: '艺术',
-            mergedCourses: [],
-            classes: [{id: 1, name: '艺术/一年级（1）班'}, {id: 2, name: '艺术/一年级（2）班'}]
-          },
-          {
-            id: 3,
-            courseName: '手工',
-            mergedCourses: [],
-            classes: [{id: 1, name: '手工/一年级（1）班'}, {id: 2, name: '手工/一年级（2）班'}]
-          }],
-        '1': [
-          {
-            id: 4,
-            courseName: '数学',
-            mergedCourses: [],
-            classes: [{id: 1, name: '数学/一年级（1）班'}, {id: 2, name: '数学/一年级（2）班'}]
-          },
-          {
-            id: 5,
-            courseName: '英语',
-            mergedCourses: [],
-            classes: [{id: 1, name: '英语/一年级（1）班'}, {id: 2, name: '英语/一年级（2）班'}]
-          },
-          {
-            id: 6,
-            courseName: '语文',
-            mergedCourses: [],
-            classes: [{id: 1, name: '语文/一年级（1）班'}, {id: 2, name: '语文/一年级（2）班'}]
-          }
-        ]
-      },
-      isMergable: true,
+      activeIndex: 0,
+      checkedClasses: [], // 选中的待合并班级
+      checkedMergedClasses: [], // 选中的合并了的班级
+      courses: [
+        {
+          id: 1,
+          courseName: '音乐',
+          classes: [
+            {id: 1, className: '一年级一班', teachers: [{id: 1, teacherName: '李明'}]},
+            {id: 2, className: '一年级二班', teachers: [{id: 1, teacherName: '李明'}]}
+          ],
+          mergedClasses: []
+        },
+        {
+          id: 2,
+          courseName: '体育',
+          classes: [
+            {id: 1, className: '一年级一班', teachers: [{id: 1, teacherName: '黄明'}]},
+            {id: 2, className: '一年级二班', teachers: [{id: 1, teacherName: '黄明'}]}
+          ],
+          mergedClasses: []
+        }
+      ],
+      isSplitable: false,
+      selectedCourse: {},
+      isMergable: false,
       isTipsShow: false
     }
   },
-  computed: {
-    getCourses () {
-      return this.courses[this.selectedTeacher]
-    }
+  created () {
+    this.selectedCourse = this.courses[0]
+    console.log(this.selectedCourse)
   },
   methods: {
-    change (option) {
-      console.log(option)
-      console.log(this.$refs.transfer)
-      this.isMergable = option.length !== 1
+    clickCourse (course, index) {
+      this.activeIndex = index
+      this.selectedCourse = course
     },
-    handleChange (value, direction, movedKeys) {
-      console.log(value, direction, movedKeys)
+    mergeClass () {
+      const classes = this.checkedClasses
+      const id = this.selectedCourse.id + '_' + this.selectedCourse.mergedClasses.length
+      const mergedClass = {id, classes}
+      this.selectedCourse.classes = this.selectedCourse.classes.filter(element => {
+        const select = classes.find(el => {
+          return el.id === element.id
+        })
+        return !select
+      })
+      this.selectedCourse.mergedClasses.push(mergedClass)
+      this.checkedClasses = []
+      this.isMergable = false
     },
-    checkSelected () {
-      this.isTipsShow = this.isMergable
-      return this.isTipsShow
+    checkMergedClass () {
+      console.log('hh')
+      this.isSplitable = true
+    },
+    checkClass () {
+      this.isMergable = this.checkedClasses.length > 1
+    },
+    splitClass () {
+      this.checkedMergedClasses.forEach(element => {
+        Array.prototype.push.apply(this.selectedCourse.classes, element.classes)
+      })
+
+      console.log(this.selectedCourse.classes)
+      this.selectedCourse.mergedClasses = this.selectedCourse.mergedClasses.filter(element => {
+        const select = this.selectedCourse.mergedClasses.find(el => {
+          return el.id === element.id
+        })
+        return !select
+      })
+      this.checkedMergedClasses = []
+      this.isSplitable = false
+    },
+    transformTeacher (teachers) {
+      return teachers.map(element => {
+        return element.teacherName
+      }).join('、')
     }
   }
 }
 </script>
+
+<style>
+.direction,
+.operation,
+.step3-container,
+.step2-container,
+.step1-container {
+  height: 300px;
+  padding-right: 10px;
+  border-right: 1px solid #eee;
+  display: inline-block;
+  text-align: center;
+}
+.direction,
+.operation {
+  vertical-align: top;
+  display: inline-flex;
+  align-items: center;
+}
+.direction {
+  border: none;
+}
+.step-arrow-right {
+  font-size: 24px;
+  font-weight: 500;
+  margin: 0 20px;
+}
+.step1-container {
+  width: 150px;
+}
+.step2-container {
+  vertical-align: top;
+  width: 250px;
+}
+.step3-container {
+  vertical-align: top;
+  border: none;
+  width: 300px;
+}
+.step2-container .el-checkbox+.el-checkbox {
+  margin-left: 0;
+}
+.course-active-item {
+  background: #409EFF;
+  color: #fff;
+  border-radius: 20px;
+}
+.course-item {
+  height: 30px;
+  line-height: 30px;
+  width: 140px;
+  margin: 10px auto;
+}
+.class-item {
+  display: block;
+  margin: 10px auto;
+}
+.search-box {
+  margin-top: 20px;
+}
+.step-title {
+  color: #F8A762;
+  width: 100px;
+}
+.class-teacher-container {
+  margin-top: 20px;
+}
+</style>
--- src/model/eventTime.js
+++ src/model/eventTime.js
@@ -4,7 +4,6 @@ export default class EventTime {
   constructor (currDate, time, weekDay, isIdle) {
     if (!currDate) {
       this.currDate = moment().weekday(weekDay)
-      console.log(this.currDate.format('YYYY-MM-DD'))
       this.currDate = moment(this.currDate)
     } else {
       this.currDate = moment(currDate)
